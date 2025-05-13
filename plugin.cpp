b08d25b7abb0386d442297ea0f8044a4a0bb7fd0
Mem0Real <juniourmimo@gmail.com>
2025-05-13 03:31:36 +0300

    Made it so that it shows 'hello world' text in console

--- plugin.cpp
+++ plugin.cpp
@@ -1,27 +1,10 @@
-#pragma once
-
-#include <spdlog/sinks/basic_file_sink.h>
-
-namespace logger = SKSE::log;
-
-void SetupLog() {
-    auto logsFolder = SKSE::log::log_directory();
-    if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
-    auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
-    auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
-    auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
-    auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
-    spdlog::set_default_logger(std::move(loggerPtr));
-    spdlog::set_level(spdlog::level::trace);
-    spdlog::flush_on(spdlog::level::trace);
-}
-
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
 
-    SetupLog();
-
-    logger::info("Just do it please.....");
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
+        if (message->type == SKSE::MessagingInterface::kDataLoaded)
+            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+    });
 
     return true;
 }
